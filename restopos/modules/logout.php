4c70e368868a070ebfe0e5bec1aa9a33ffe54b67
Aski Ahamed <88615751+Aski49@users.noreply.github.com>
2026-06-22 09:36:30 +0530

    Delete restopos directory

--- restopos/modules/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once '../includes/config.php';
-if (isLoggedIn()) {
-    $u = currentUser();
-    logActivity('Logged Out', 'auth', ($u['name'] ?? 'User').' signed out');
-}
-session_destroy();
-header('Location: ../index.php');
-exit;
